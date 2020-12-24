d8e26f9cac857b12a95d192bd1e3e63c9d276790
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-24 14:34:08 -0300

    infra: export register-iam-role to SSM parameter store

--- terraform/infra/users/iam-role-register.tf
+++ terraform/infra/users/iam-role-register.tf
@@ -17,3 +17,9 @@ resource "aws_iam_role" "register_iam_role" {
   }
   EOF
 }
+
+resource "aws_ssm_parameter" "register_iam_role" {
+  name  = "${var.environment}-register-iam_role"
+  type  = "String"
+  value = "${aws_iam_role.register_iam_role.arn}"
+}
