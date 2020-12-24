b1abadd2269b039d912173056246ac6e07321211
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-24 16:31:39 -0300

    infra: add email Global Secondary Index (GSI) to users table

--- terraform/infra/users/dynamodb-users-.tf
+++ terraform/infra/users/dynamodb-users-.tf
@@ -7,10 +7,29 @@ resource "aws_dynamodb_table" "users" {
   }
   write_capacity = "${var.write_capacity}"
   read_capacity  = "${var.read_capacity}"
+
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "${var.environment}-email-gsi"
+    projection_type = "ALL"
+    hash_key        = "email"
+    write_capacity  = "${var.write_capacity}"
+    read_capacity   = "${var.read_capacity}"
+  }
 }
 
 resource "aws_ssm_parameter" "dynamodb_users_table" {
   name  = "${var.environment}-dynamodb-users-table"
   type  = "String"
   value = "${aws_dynamodb_table.users.name}"
+}
+
+resource "aws_ssm_parameter" "email-gsi" {
+  name  = "${var.environment}-email-gsi"
+  type  = "String"
+  value = "${var.environment}-email-gsi"
 }
