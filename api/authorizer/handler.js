21bc548ef87c8509160a75a2c5272ff5f41b722e
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-24 18:11:49 -0300

    feat: implement lambda authorizer

---
+++ api/authorizer/handler.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+exports.authorizer = function(event, context, callback) {
+  const token = event.authorizationToken;
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    callback(null, generatePolicy('user', 'Allow', event.methodArn, user));
+  } catch (exception) {
+    console.log(exception);
+    callback(null, generatePolicy('user', 'Deny', event.methodArn));
+  }
+};
+
+const generatePolicy = function(principalId, effect, resource, user) {
+  let authResponse = {};
+  
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+      let policyDocument = {};
+      policyDocument.Version = '2012-10-17'; 
+      policyDocument.Statement = [];
+      let statementOne = {};
+      statementOne.Action = 'execute-api:Invoke'; 
+      statementOne.Effect = effect;
+      statementOne.Resource = resource;
+      policyDocument.Statement[0] = statementOne;
+      authResponse.policyDocument = policyDocument;
+  }
+  
+  if (user) {
+    authResponse.context = user;
+  }
+  return authResponse;
+}
