6e0b16fc8540ae10f0f07fec72a7d8de3460067e
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-24 14:26:54 -0300

    infra: create IAM policy attachment, policy and role to user register lambda function

---
+++ terraform/infra/users/iam-policy-register.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_policy" "register_policy" {
+  name   = "${var.environment}-register_policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:PutItem*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_dynamodb_table.users.arn}"
+    }
+  ]
+}
+EOF
+}
---
+++ terraform/infra/users/iam-role-register.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "register_iam_role" {
+  name = "${var.environment}-register-iam-role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
