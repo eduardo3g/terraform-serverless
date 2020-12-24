efd9b1e532c912643dd07aecb91acf536175df12
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-24 13:24:01 -0300

    infra: delete hello-world files from environments and infra directories

--- terraform/environments/dev/main.tf
+++
@@ -1,6 +0,0 @@
-module "hello" {
-  source         = "../../infra/hello"
-  environment    = "${var.environment}"
-  write_capacity = 1
-  read_capacity  = 1
-}
--- terraform/environments/prod/main.tf
+++
@@ -1,6 +0,0 @@
-module "hello" {
-  source         = "../../infra/hello"
-  environment    = "${var.environment}"
-  write_capacity = 10
-  read_capacity  = 10
-}
