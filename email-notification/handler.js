526842e4c9e8447786af55c284cbf43c0b6c4766
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 18:59:53 -0300

    feat: consume email SQS queue and send mail to user with reservation confirmation

--- email-notification/handler.js
+++ email-notification/handler.js
@@ -1,7 +1,35 @@
 'use strict';
 
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_SERVER,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_FROM_PASSWORD,
+  },
+});
+
 module.exports.send = async event => {
-  console.log(JSON.stringify(event));
+  let emailPromises = [];
+
+  for (let record of event.Records) {
+    const message = JSON.parse(record.body).Message;
+
+    emailPromises.push(transporter.sendMail({
+      from: `"Reservations 👻" <${process.env.EMAIL_FROM}>`,
+      to: process.env.EMAIL_TO,
+      subject: "Reservation confirmed ✔",
+      text: message,
+      html: message,
+    }));
+  }
+
+  await Promise.all(emailPromises);
+  
+  console.log('All the email were sent successfully');  
 
   return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
