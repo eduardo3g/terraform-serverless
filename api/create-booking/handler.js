9bb4fc84e62ff76903458257957d17f1ed5179cb
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 13:34:31 -0300

    feat: implement create-bookings lambda function

--- api/create-booking/handler.js
+++ api/create-booking/handler.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: process.env.AWS_REGION,
+});
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const { v4: uuidv4 } = require('uuid');
+
 module.exports.create = async event => {
+  const body = JSON.parse(event.body);
+
+  await documentClient.put({
+    TableName: process.env.DYNAMODB_BOOKINGS,
+    Item: {
+      id: uuidv4(),
+      date: body.date,
+      user: event.requestContext.authorizer,
+    }
+  }).promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(event),
+    body: JSON.stringify({ message: 'The booking was created successfully' }),
   }
 };
