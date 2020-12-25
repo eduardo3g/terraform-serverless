b8c86f0f3264d4103168dd078fe40598c4e5354d
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 14:28:04 -0300

    infra: enable DynamoDB streams on bookings DynamoDB table

--- terraform/infra/bookings/dynamodb-bookings.tf
+++ terraform/infra/bookings/dynamodb-bookings.tf
@@ -5,8 +5,10 @@ resource "aws_dynamodb_table" "bookings" {
     name = "id"
     type = "S"
   }
-  write_capacity = "${var.write_capacity}"
-  read_capacity  = "${var.read_capacity}"
+  write_capacity   = "${var.write_capacity}"
+  read_capacity    = "${var.read_capacity}"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
 }
 
 resource "aws_ssm_parameter" "dynamodb_bookings_table" {
