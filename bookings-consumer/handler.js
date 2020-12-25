58766e4b17136f9a75378d1927f48a06b540eefe
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 15:25:39 -0300

    feat: format stream timestamp into a message and push it to Cloudwatch logs

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -2,11 +2,19 @@
 
 const AWS = require('aws-sdk');
 const converter = AWS.DynamoDB.Converter;
+const moment = require('moment');
+
+moment.locale('pt-br');
 
 module.exports.listen = async event => {
   for (let record of event.Records) {
-    console.log('Parsed JSON', converter.unmarshall(record.dynamodb.NewImage));
+    if (record.eventName === 'INSERT') {
+      const booking = converter.unmarshall(record.dynamodb.NewImage);
+      console.log(
+        `Booking created: the user ${booking.user.name} (${booking.user.email}) created a booking on: ${moment(booking.date).format('LLLL')}`
+      );
+    }
   }
-  
+
   return {  message: 'Go Serverless v1.0! Your function executed successfully!' };
 };
