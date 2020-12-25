f135255cecdbeced659a65e8f9c614f2160b0edc
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 15:17:17 -0300

    feat: use unmarshall method of DynamoDB.Converter module, to convert record.dynamodb.NewImage to common json

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const converter = AWS.DynamoDB.Converter;
+
 module.exports.listen = async event => {
-  console.log(JSON.stringify(event));
+  for (let record of event.Records) {
+    console.log('Parsed JSON', converter.unmarshall(record.dynamodb.NewImage));
+  }
+  
   return {  message: 'Go Serverless v1.0! Your function executed successfully!' };
 };
