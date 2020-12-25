1bde3aecf1c654e9d33317bac70dc21d93bb5c70
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 16:45:10 -0300

    feat: publish messages to SNS notifications topic

--- bookings-consumer/handler.js
+++ bookings-consumer/handler.js
@@ -1,20 +1,32 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+AWS.config.update({
+  region: process.env.AWS_REGION,
+});
+const SNS = new AWS.SNS();
 const converter = AWS.DynamoDB.Converter;
 const moment = require('moment');
 
 moment.locale('pt-br');
 
 module.exports.listen = async event => {
+  const snsPromises = [];
+
   for (let record of event.Records) {
     if (record.eventName === 'INSERT') {
       const booking = converter.unmarshall(record.dynamodb.NewImage);
-      console.log(
-        `Booking created: the user ${booking.user.name} (${booking.user.email}) created a booking on: ${moment(booking.date).format('LLLL')}`
-      );
+
+      snsPromises.push(SNS.publish({
+        TopicArn: process.env.SNS_NOTIFICATIONS_TOPIC,
+        Message: `Booking created: the user ${booking.user.name} (${booking.user.email}) created a booking on: ${moment(booking.date).format('LLLL')}`,
+      }).promise());
     }
   }
+
+  await Promise.all(snsPromises);
+
+  console.log('Message(s) sent successfully to SNS topic');
 
   return {  message: 'Go Serverless v1.0! Your function executed successfully!' };
 };
