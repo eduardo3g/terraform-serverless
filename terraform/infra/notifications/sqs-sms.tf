1d48d0dbec90d87ecb723d779d962a2199e97e4d
eduardo3g <eduardosbrasil10@gmail.com>
2020-12-25 17:18:03 -0300

    infra: create policy to SQS sms queue

--- terraform/infra/notifications/sqs-sms.tf
+++ terraform/infra/notifications/sqs-sms.tf
@@ -4,6 +4,10 @@ resource "aws_sqs_queue" "sms" {
     deadLetterTargetArn = aws_sqs_queue.sms_dlq.arn
     maxReceiveCount     = 3
   })
+  policy         = templatefile("${path.module}/templates/sqs-sns-policy.tpl", {
+    resource = "arn:aws:sqs:${var.region}:${var.account_id}:${var.environment}-sms-queue"
+    source_arn = "${aws_sns_topic.notifications.arn}"
+  })
 }
 
 resource "aws_sqs_queue" "sms_dlq" {
